0b1f21f97f40156a81c37c702f55eb63a951c27c
HassanChe25 <hassanche101@gmail.com>
2025-11-29 18:49:17 +0100

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = { 
-    'default': { 
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'demo', 
-        'USER': 'postgres', 
-        'PASSWORD': '0799201180', 
-        'HOST': '127.0.0.1', 
-        'PORT': '5432', 
-    } 
-} 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'npg_pShcB8rKku5t',
+        'HOST': 'ep-solitary-dawn-a26alla3.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
+    }
+}
 
 
 # Password validation
